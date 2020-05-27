5f49f78e032789c7abfa0559da1a0c62e465a40b
Jonathan East <jonathan_east@ymail.com>
2020-05-27 22:01:21 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,18 @@
 class Transfer
   # your code here
+
+  attr_accessor :amount, :status
+  attr_reader :sender, :receiver
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+
+  end
+
 end
