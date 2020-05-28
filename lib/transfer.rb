9422ef100315e931fecc04a2ebf9d1722679b823
Jonathan East <jonathan_east@ymail.com>
2020-05-28 00:43:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,9 +16,9 @@ class Transfer
   end
 
   def execute_transaction
+    sender.balance-=amount
+    receiver.balance+=amount
     if @status == "pending" && sender.valid?
-      sender.balance-=amount
-      receiver.balance+=amount
       @status = "complete"
     else
       "Transaction rejected. Please check your account balance."
